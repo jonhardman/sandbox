97b8712ed81086af2b1443b4e62811ed129463db
Jon Hardman <jonhardman@gmail.com>
2015-07-17 13:35:38 +0100

    done

--- ui5_app_reverse_proxy/Server.js
+++ ui5_app_reverse_proxy/Server.js
@@ -1,8 +1,18 @@
 var express = require('express')
 var app = express()
-var proxy   = require( 'http-proxy' ).createProxyServer;
+var request = require('request');
 
 app.use(express.static(__dirname+'/www/'))
-app.listen(5000)
+app.listen(5001)
 
-//Mark reverse proxy stuff here
+//reverse proxy stuff here
+//does not work with $metadata
+app.get('/sap/opu/odata/CITEXP/EXPENSES_SRV/', function (req,res){
+request.get('http://q12gw20.eai-ltd.co.uk:8012/sap/opu/odata/CITEXP/EXPENSES_SRV/$metadata', {
+  'auth': {
+    'user': 'mkelsall',
+    'pass': 'pr0file1',
+    'sendImmediately': true
+  }
+}).pipe(res);
+})
